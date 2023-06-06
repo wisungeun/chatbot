b0e22c31596c8892f64e86288085174012a85471
wisungeun <sungeunwi@naver.com>
2023-06-06 18:43:12 +0900

    code test 완료

--- report14(chatbot).py
+++ report14(chatbot).py
@@ -1,91 +1,63 @@
 import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.pairwise import cosine_similarity
+import time
 
-
-
-# 레벤슈타인 거리 구하기
-def calc_distance(a, b):
-    ''' 레벤슈타인 거리 계산하기 '''
-    if a == b: return 0 # 같으면 0을 반환
-    a_len = len(a) # a 길이
-    b_len = len(b) # b 길이
+def calc_distance(a,b):
+    if a == b: return 0
+    a_len = len(a)
+    b_len = len(b)
     if a == "": return b_len
     if b == "": return a_len
-    # 2차원 표 (a_len+1, b_len+1) 준비하기 --- (※1)
-    # matrix 초기화의 예 : [[0, 1, 2, 3], [1, 0, 0, 0, 0], [2, 0, 0, 0, 0], [3, 0, 0, 0, 0], [4, 0, 0, 0, 0]]
-    # [0, 1, 2, 3]
-    # [1, 0, 0, 0]
-    # [2, 0, 0, 0]
-    # [3, 0, 0, 0] 
-    matrix = [[] for i in range(a_len+1)] # 리스트 컴프리헨션을 사용하여 1차원 초기화
-    for i in range(a_len+1): # 0으로 초기화
-        matrix[i] = [0 for j in range(b_len+1)]  # 리스트 컴프리헨션을 사용하여 2차원 초기화
-    # 0일 때 초깃값을 설정
+    
+    matrix = [[] for i in range(a_len+1)]
+    for i in range(a_len+1):
+        matrix[i] = [0 for j in range(b_len+1)]
+    
     for i in range(a_len+1):
         matrix[i][0] = i
     for j in range(b_len+1):
         matrix[0][j] = j
-    # 표 채우기 --- (※2)
-    # print(matrix,'----------')
+        
     for i in range(1, a_len+1):
         ac = a[i-1]
-        # print(ac,'=============')
         for j in range(1, b_len+1):
-            bc = b[j-1] 
-            # print(bc)
-            cost = 0 if (ac == bc) else 1  #  파이썬 조건 표현식 예:) result = value1 if condition else value2
+            bc = b[j-1]
+            cost = 0 if (ac == bc) else 1
             matrix[i][j] = min([
-                matrix[i-1][j] + 1,     # 문자 제거: 위쪽에서 +1
-                matrix[i][j-1] + 1,     # 문자 삽입: 왼쪽 수에서 +1   
-                matrix[i-1][j-1] + cost # 문자 변경: 대각선에서 +1, 문자가 동일하면 대각선 숫자 복사
-            ])
-            # print(matrix)
-        # print(matrix,'----------끝')
-    return matrix[a_len][b_len]
-# "얼마나 분석이 될까요"와 "유사도나 분석 할까요"의 거리 --- (※3)
-print(calc_distance("얼마나 분석이 될까요","유사도나 분석 할까요"))
-# 실행 예
-# samples = ["신촌역","신천군","신천역","신발","마곡역"]
-# base = samples[0]
-# r = sorted(samples, key = lambda n: calc_distance(base, n))  # samples 리스트의 각 요소에 대해 calc_distance(base, n) 함수를 호출하여 레벤슈타인 거리를 계산하고, 이를 기준으로 리스트를 정렬
-# for n in r:
-#     print(calc_distance(base, n), n)
-    
-    
-    
-class SimpleChatBot:
-
-    def __init__(self, filepath):
-        self.questions, self.answers = self.load_data(filepath)
-        self.vectorizer = TfidfVectorizer()
-        self.question_vectors = self.vectorizer.fit_transform(self.questions)  # 질문을 TF-IDF로 변환
-
-    def load_data(self, filepath):
-        data = pd.read_csv(filepath)
-        questions = data['Q'].tolist()  # 질문열만 뽑아 파이썬 리스트로 저장
-        answers = data['A'].tolist()   # 답변열만 뽑아 파이썬 리스트로 저장
-        return questions, answers
-
-    def find_best_answer(self, input_sentence):
-        input_vector = self.vectorizer.transform([input_sentence])
-        similarities = cosine_similarity(input_vector, self.question_vectors) # 코사인 유사도 값들을 저장
+                matrix[i-1][j] + 1,
+                matrix[i][j-1] + 1,
+                matrix[i-1][j-1] + cost
+                ])
         
-        best_match_index = similarities.argmax()   # 유사도 값이 가장 큰 값의 인덱스를 반환
-        return self.answers[best_match_index]
+    
+    return matrix[a_len][b_len]
+    
 
-# CSV 파일 경로를 지정하세요.
-filepath = 'ChatbotData.csv'
+def load_data(filepath):
+    data = pd.read_csv(filepath)
+    questions1 = data['Q'].tolist()
+    answers1 = data['A'].tolist()
+    return questions1, answers1
 
-# 간단한 챗봇 인스턴스를 생성합니다.
-chatbot = SimpleChatBot(filepath)
+    
 
-# '종료'라는 단어가 입력될 때까지 챗봇과의 대화를 반복합니다.
+
+# CSV 파일 경로
+filepath = 'C:\Coding\Python\chatbot\chatbot\ChatbotData.csv'
+
+questions, answers = load_data(filepath)
+
+q_len = len(questions)
+q_r = []
+
 while True:
     input_sentence = input('You: ')
     if input_sentence.lower() == '종료':
         break
-    response = chatbot.find_best_answer(input_sentence)
-    print('Chatbot:', response)
+    for i in range(q_len):
+        r = calc_distance(input_sentence, questions[i])
+        q_r.append(r)
     
-  
+    r = q_r.index(min(q_r))
+    print(answers[r])
+    q_r = []
+    
